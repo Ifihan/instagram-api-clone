22d49de4dce3865901df349158767a25ad2eb19f
Ifihan <victoriaolusheye@gmail.com>
2021-11-17 00:55:21 +0100

    Added features to the comments app

--- comments/urls.py
+++ comments/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from comments.views import CommentsView
+from comments.views import CreateCommentView, ListCommentView, UpdateCommentView, DeleteCommentView
 
 urlpatterns = [
-    path('', CommentsView.as_view(), name='comments'),
+    path('', CreateCommentView.as_view(), name='comments'),
+    path('<int:pk>', ListCommentView.as_view(), name='list-comments'),
+    path('update/<int:pk>/', UpdateCommentView.as_view(), name='update-comment'),
+    path('delete/<int:pk>/', DeleteCommentView.as_view(), name='delete-comment')
 ]
--- comments/views.py
+++ comments/views.py
@@ -2,37 +2,48 @@ from django.shortcuts import render
 
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
 from comments.models import Comments
 from comments.serializers import CommentsSerializer
 # Create your views here.
 
-class CommentsView(APIView):
-    # permission_classes = (IsAuthenticated,)
-    def get(self, request, format=None):
+class CreateCommentView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        serializer = CommentsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ListCommentView(APIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get(self, request):
         comments = Comments.objects.all()
         serializer = CommentsSerializer(comments, many=True)
         return Response(serializer.data)
 
-    def post(self, request, format=None):
-        serializer = CommentsSerializer(data=request.data)
+class UpdateCommentView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def put(self, request, pk):
+        comment = Comments.objects.get(pk=pk)
+        serializer = CommentsSerializer(comment, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class CommentsViewDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Comments.objects.get(pk=pk)
-        except Comments.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+class DeleteCommentView(APIView):
+    permission_classes = (IsAuthenticated,)
 
-    def get(self, request, pk, format=None):
-        comments = self.get_object(pk)
-        serializer = CommentsSerializer(comments)
-        return Response(serializer.data)
+    def delete(self, request, pk):
+        comment = Comments.objects.get(pk=pk)
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 # liked comments feed view
